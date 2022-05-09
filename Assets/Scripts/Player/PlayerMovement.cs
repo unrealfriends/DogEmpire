4c3cb296c7c22f345c954a4ea56277017975c4fb
unrealfriends <lisamortsch98@gmail.com>
2022-05-09 22:00:14 +0200

    player walking animation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,13 +8,15 @@ public class PlayerMovement : MonoBehaviour
     //private Animator mAnimator;
     public NavMeshAgent mNavMeshAgent;
 
-    private bool mRunning = false;
     public Camera playerCamera;
+
+    public Animator playerAnimator;
+    public bool isWalking;
     // Start is called before the first frame update
     void Start()
     {
         //mAnimator = GetComponent<Animator>();
-        mNavMeshAgent = GetComponent<NavMeshAgent>();
+        //mNavMeshAgent = GetComponent<NavMeshAgent>();
 
     }
 
@@ -25,22 +27,24 @@ public class PlayerMovement : MonoBehaviour
             RaycastHit hit;
         if (Input.GetMouseButton(0))
         {
-            print ("HELLO");
             if (Physics.Raycast(ray, out hit, 100))
             {
                 mNavMeshAgent.destination = hit.point;
             }
         }
 
-        //if (mNavMeshAgent.remainingDistance <= mNavMeshAgent.stoppingDistance)
-        //{
-        //    mRunning = false;
-        //}
-        //else
-        //{
-        //    mRunning = true;
-        //}
-
-        //mAnimator.SetBool("running", mRunning);
+        // wenn ziel erreicht ist
+        if (mNavMeshAgent.remainingDistance <= mNavMeshAgent.stoppingDistance)
+        {
+            // stoppen
+            isWalking = false;
+        }
+        else
+        {
+            // sonst weitegehen
+            isWalking = true;
+        }
+        // bool im animator entsprechend setzen
+        playerAnimator.SetBool("isWalking", isWalking);
     }
 }
