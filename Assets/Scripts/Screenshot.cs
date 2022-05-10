7014b0af8bf338eb953efd9ee0b76abd829d957f
unrealfriends <lisamortsch98@gmail.com>
2022-05-10 14:19:59 +0200

    screenshot + small fixes
    for inventory

--- Assets/Scripts/Screenshot.cs
+++ Assets/Scripts/Screenshot.cs
@@ -4,39 +4,15 @@ using UnityEngine;
 
 public class Screenshot : MonoBehaviour
 {
-//    public Camera camera;
-//    public string prefix;
-//    public string pathFolder;
-//    void TakeScreenshot(string fullPath)
-//    {
-//        if (camera == null)
-//        {
-//            camera = GetComponent<Camera>;
-//        }
+		public int superSize = 2;
+		private int _shotIndex = 0;
 
-//        RenderTexture rt = new RenderTexture(256, 256, 24);
-//        camera.targetTexture = rt;
-//        Texture2D screenShot = new Texture2D(256, 256, TextureFormat.RGBA32, false);
-//        camera.Render();
-//        RenderTexture.active = rt;
-//        screenShot.ReadPixels(new Rect(0, 0, 256, 256), 0, 0);
-//        camera.targetTexture = null;
-//        RenderTexture.active = null;
-
-//        if (Application.isEditor)
-//        {
-//            DestroyImmediate(rt);
-//        }
-//        else
-//        {
-//            Destroy(rt);
-//        }
-//        byte[] bytes = screenShot.EncodeToPNG();
-//        System.IO.File.WriteAllBytes(fullPath, bytes);
-
-//#if UNITY_EDITOR
-//Asset.Database.Refresh();
-//#endif
-    //}
-
+		private void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.A))
+			{
+				ScreenCapture.CaptureScreenshot($"Screenshot{_shotIndex}.png", superSize);
+				_shotIndex++;
+			}
+		}
 }
